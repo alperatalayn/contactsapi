4abf30791a89fcae04837cafa157f90ee19f4992
alperatalayn <alperatalayn@gmail.com>
2021-01-30 13:26:57 +0300

    adresses update bugfix

--- api/serializers.py
+++ api/serializers.py
@@ -20,14 +20,14 @@ class ContactSerializer(serializers.ModelSerializer):
             Adress.objects.create(contact=contact, **adress_data)
         return contact
     def update(self, instance, validated_data):
-        #TODO: fix here!
         adresses_data = validated_data.pop('adresses')
         adresses = instance.adresses
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.phone = validated_data.get('phone', instance.phone)
         instance.job = validated_data.get('job', instance.job)
-        adresses.delete()
+        for adress in adresses.all():
+            adress.delete()
         for adress_data in adresses_data:
             Adress.objects.create(contact=instance, **adress_data)
         instance.save()
