31627e1e779359a3829d2601a1b99c51fc43e301
alperatalayn <alperatalayn@gmail.com>
2021-02-05 10:43:50 +0300

    pdf report

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('contact-list/',views.contactList,name="contact-list"),
+    path('pdf-report/',views.pdfReport,name="report"),
     path('create/',views.createContact,name="create"),
     path('update/<str:pk>',views.updateContact,name="update"),
     path('delete/<str:pk>',views.deleteContact,name="delete"),
--- api/views.py
+++ api/views.py
@@ -4,12 +4,69 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsOwnerOrAdmin
-from rest_framework import status,views
+from rest_framework import status, views
 from .models import Contact
-from .serializers import ContactSerializer,RegisterSerializer
+from .serializers import ContactSerializer, RegisterSerializer
 import json
-# Create your views here.
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter, inch
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def pdfReport(request):
+    try:
+        buffer = io.BytesIO()
+        pdf = canvas.Canvas(buffer)
+        data = [["Ad","Soyad","Telefon No."]]
+        if request.user.is_superuser: 
+            contacts = Contact.objects.all()
+        else:
+            contacts = Contact.objects.filter(owner=request.user)
+        for contact in contacts:
+            data.append([contact.first_name,contact.last_name,contact.phone])
+
+        table = Table(data,colWidths=570/3)
+
+        style = TableStyle([
+            ('BACKGROUND',(0,0),(-1,0),colors.aliceblue),
+            ('TEXTCOLOR',(0,0),(-1,0),colors.black),
+            ('FONTNAME',(0,0),(-1,0),'Courier-Bold'),
+            ('FONTSIZE',(0,0),(-1,0),22),
+            ('BOTTOMPADDING',(0,0),(-1,0),20),
+        ])
+        table.setStyle(style)
+
+        rowNum = len(data)
+        for i in range(1,rowNum):
+            if i%2 == 0:
+                bc = colors.burlywood
+            else:
+                bc = colors.beige
+            ts = TableStyle(
+                [('BACKGROUND',(0,i),(-1,i),bc)]
+            )
+            table.setStyle(ts)
+        ts = TableStyle(
+                [
+                    ('BOX',(0,0),(-1,-1),2,colors.black),
+                    ('GRID',(0,1),(-1,-1),2,colors.black)
+                ]
+            )
+        table.setStyle(ts)
+        table.wrapOn(pdf,1,1)
+        table.drawOn(pdf,10,832-table._height)
+        pdf.showPage()
+        pdf.save()
+        buffer.seek(0)
+        return FileResponse(buffer,filename="report.pdf",as_attachment=True)
+    except:
+        raise Exception("An error occured")
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
